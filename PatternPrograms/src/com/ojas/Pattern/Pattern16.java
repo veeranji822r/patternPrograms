a39a7458779da29e9dc94ed27539201a19386759
veeranji822r <vr21035@OJAS-DD27.corp.ojas-it.com>
2021-02-13 13:11:56 +0530

    changes done at 13-2-2021

--- PatternPrograms/src/com/ojas/Pattern/Pattern16.java
+++ PatternPrograms/src/com/ojas/Pattern/Pattern16.java
@@ -3,23 +3,42 @@ package com.ojas.Pattern;
 public class Pattern16 {
 
 	public static void main(String[] args) {
-		int num = 4;
-		int number = 0;
-		
-		for(int row=1;row<=num;row++)
-		{
-			String show = "";
-		    for(int space=1;space<=num-row;space++) 
-		    {
-		         System.out.printf(" ");
-		    }
-		    for(int col=1;col<=row;col++) 
-		    {
-		    	number +=1;
-		        show=number+show;
-		    }
-		    System.out.println(show);
-		 }
-	}
-
+//		int num = 4;
+//		int number = 0;
+//		
+//		for(int row=1;row<=num;row++)
+//		{
+//			String show ="";
+//		    for(int space=1;space<=num-row;space++) 
+//		    {
+//		         System.out.printf(" ");
+//		    }
+//		    for(int col=1;col<=row;col++) 
+//		    {
+//		    	number +=1;
+//		        show=number+show;
+//		    }
+//		    System.out.println(show);
+//		 }
+		   int number = 1, k = 0;
+	        for (int rows = 1; rows <= 4; rows++) 
+	        {
+	            for (int columns = 4; columns >= 1; columns--) 
+	            {
+	                if (rows >= columns) 
+	                {
+	                    System.out.print(number + " ");
+	                    if (columns != 1)
+	                        number--;
+	                } 
+	                else 
+	                {
+	                    System.out.print("  ");
+	                }
+	            }
+	            k = k + 2;
+	            number = number + k;
+	            System.out.println();
+	        }
+	    }
 }
